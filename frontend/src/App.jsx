53978f1666d9c6a4f4d070529dff8a15e2583164
Oscar Nilsson <oscar@osuka.dev>
2022-06-14 15:19:04 +0200

    frontend solution added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,59 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [result, setResult] = useState({ message: "" });
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleLogin = async () => {
+    const result = await axios.post("http://localhost:4000/login", credentials);
+    setResult(result.data);
+  };
+
+  const handleLogout = async () => {
+    const result = await axios.post("http://localhost:4000/logout");
+    setResult(result.data);
+  }
+
+  const handleTest = async () => {
+    const result = await axios.post("http://localhost:4000/test")
+    console.log(result.data);
+    setResult(result.data);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <p>Message: {result.message}</p>
+        <p>Session ID: {result.session_id ? result.session_id : "none"}</p>
+        <input
+          name="username"
+          id="username"
+          onChange={(e) =>
+            setCredentials({ ...credentials, username: e.target.value })
+          }
+        />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          onChange={(e) =>
+            setCredentials({ ...credentials, password: e.target.value })
+          }
+        />
+        <button onClick={handleLogin}>Login</button>
+        <button onClick={handleTest}>Test session</button>
+        <button onClick={handleLogout}>Logout</button>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
