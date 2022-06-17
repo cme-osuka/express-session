c53e0afe9d3c323efa30bf32b0c90e487508130a
Oscar Nilsson <oscar@osuka.dev>
2022-06-17 12:05:11 +0200

    removed solution

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,13 @@
 import { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
-
 function App() {
-  const [result, setResult] = useState({ message: "" });
-  const [credentials, setCredentials] = useState({
-    username: "",
-    password: "",
-  });
-
-  const handleLogin = async () => {
-    const result = await axios.post("http://localhost:4000/login", credentials);
-    setResult(result.data);
-  };
-
-  const handleLogout = async () => {
-    const result = await axios.post("http://localhost:4000/logout");
-    setResult(result.data);
-  }
-
-  const handleTest = async () => {
-    const result = await axios.post("http://localhost:4000/test")
-    console.log(result.data);
-    setResult(result.data);
-  }
-
+  
   return (
     <div className="App">
       <header className="App-header">
-        <p>Message: {result.message}</p>
-        <p>Session ID: {result.session_id ? result.session_id : "none"}</p>
-        <input
-          name="username"
-          id="username"
-          onChange={(e) =>
-            setCredentials({ ...credentials, username: e.target.value })
-          }
-        />
-        <input
-          type="password"
-          name="password"
-          id="password"
-          onChange={(e) =>
-            setCredentials({ ...credentials, password: e.target.value })
-          }
-        />
-        <button onClick={handleLogin}>Login</button>
-        <button onClick={handleTest}>Test session</button>
-        <button onClick={handleLogout}>Logout</button>
+       {/* Din kod här */}
       </header>
     </div>
   );
